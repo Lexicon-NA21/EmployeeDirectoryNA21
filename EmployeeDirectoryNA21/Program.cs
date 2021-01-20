db33300e8e745f5b7ca0372127b75dbe25b62c55
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-01-20 14:30:27 +0100

    Employee

--- EmployeeDirectoryNA21/Program.cs
+++ EmployeeDirectoryNA21/Program.cs
@@ -6,24 +6,7 @@ namespace EmployeeDirectoryNA21
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine();
-            int x = 5;
-            Robot robot0 = new Robot("Kalle");
-            Robot robot1 = new Robot("Anna");
-            //Robot robot2 = new Robot();
-            //Robot robot3 = new Robot();
-
-            robot0.Length = 5;
-            int length = robot0.Length;
-
-            string robotName = robot0.Name;
-
-            robot0.Name = "Kalle";
-            robot0.Walk(10);
-
-           // robot0.age = 5;
-
+            Employee kalle = new Employee( 10000, "Kalle");
         }
     }
 }
