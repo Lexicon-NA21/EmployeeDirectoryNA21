18cbdb767c336ea1877d5ec7f6d4ee0cd30d69db
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-01-20 15:03:38 +0100

    Day one finished!

---
+++ EmployeeDirectoryNA21/Payroll.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeDirectoryNA21
+{
+    class Payroll
+    {
+        private List<Employee> payroll;
+
+        public Payroll()
+        {
+            payroll = new List<Employee>();
+        }
+
+        public Employee[] GetEmployees()
+        {
+            return payroll.ToArray();
+        }
+
+        internal void AddEmplyee(string name, int salary)
+        {
+            payroll.Add(new Employee(salary, name));
+        }
+    }
+}
--- EmployeeDirectoryNA21/Program.cs
+++ EmployeeDirectoryNA21/Program.cs
@@ -4,9 +4,25 @@ namespace EmployeeDirectoryNA21
 {
     class Program
     {
+        static Payroll payroll = new Payroll();
+
         static void Main(string[] args)
         {
-            Employee kalle = new Employee( 10000, "Kalle");
+            SeedData();
+
+            Employee[] employees = payroll.GetEmployees();
+
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine($"Name: {employee.Name} Salary: {employee.Salary}");
+            }
+        }
+
+        private static void SeedData()
+        {
+            payroll.AddEmplyee("Kalle", 10000);
+            payroll.AddEmplyee("Anna", 20000);
+            payroll.AddEmplyee("Lisa", 30000);
         }
     }
 }
